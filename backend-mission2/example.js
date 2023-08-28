cf747ec1dafc3fd36affd84bc71bc0b7ac9f9dec
John Doe <patricktricenio549@gmail.com>
2023-08-28 15:28:26 +1200

    main

--- backend-mission2/example.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3001;
-
-app.get('/', (req, res) => res.send('App is running fine'));
-app.listen(port, () => console.log('Application Started'));
